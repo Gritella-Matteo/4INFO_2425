a08870b644bbcd9a9c17fcca1f5a80de8755d6f3
Gritella-Matteo <147144328+Gritella-Matteo@users.noreply.github.com>
2025-04-04 13:46:08 +0200

    consegno es web app

---
+++ TPSI/matteo_gritella/svelte/WebApp/src/lib/components/who_am_i.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let nome="Matteo";
+    export let cognome="Gritella";
+    export let eta=17;
+    export let width=20;
+</script>
+
+
+<h1>   Ciao io sono {nome} {cognome} ed ho {eta} anni </h1>
+<h4> sono uno studente di 4 INFO presso l'istituto agnelli di torino</h4>
+<img src={"https://ats.agnelli.it/img/team/avatar-canale.png"} alt="Matteo" width={width}>
---
+++ TPSI/matteo_gritella/svelte/WebApp/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>WELCOME to my Web App</h1>
+
