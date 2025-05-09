420acc0081a6f26af2e89264489e4d0e933013ff
Gritella-Matteo <147144328+Gritella-Matteo@users.noreply.github.com>
2025-05-09 13:25:33 +0200

    Delete TPSI/matteo_gritella/svelte directory

--- TPSI/matteo_gritella/svelte/src/routes/about/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import Counter from "$lib/components/counter.svelte";
-    let contator=0
-</script>
-<h1>Questa è la pagina about!</h1>
-<h2>il contatore vale: {contator}</h2>
-
-<Counter bind:counter={contator}/>
